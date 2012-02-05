660fcbf544f2e39f4ae47a308f956db44f82b771
Joel Besada <joel.besada@gmail.com>
2012-02-06 00:17:48 +0100

    Updated demo to include animations

--- script/demo.js
+++ script/demo.js
@@ -4,39 +4,63 @@ function init() {
 	/* ========== DRAWING THE PATH AND INITIATING THE PLUGIN ============= */
 
 	var path = $.fn.scrollPath("getPath");
-	// Move to .demo
-	path.moveTo(400, 50);
-	// Line to .description
-	path.lineTo(400, 800);
-	// Arc down and line to .syntax
+	// Move to 'start' element
+	path.moveTo(400, 50, {name: "start"});
+	// Line to 'description' element
+	path.lineTo(400, 800, {name: "description"});
+	// Arc down and line to 'syntax'
 	path.arc(200, 1200, 400, -Math.PI/2, Math.PI/2, true);
-	path.lineTo(600, 1600, {callback: function() {
+	path.lineTo(600, 1600, {
+		callback: function() {
 			highlight($(".settings"));
-		}
+		},
+		name: "syntax"
 	});
-	// Continue line to .scrollbar
-	path.lineTo(1750, 1600, {callback: function() {
+	// Continue line to 'scrollbar'
+	path.lineTo(1750, 1600, {
+		callback: function() {
 			highlight($(".sp-scroll-handle"));
-		}
+		},
+		name: "scrollbar"
 	});
 	// Arc up while rotating
 	path.arc(1800, 1000, 600, Math.PI/2, 0, true, {rotate: Math.PI/2 });
-	// Line to .rotations
-	path.lineTo(2400, 750);
+	// Line to 'rotations'
+	path.lineTo(2400, 750, {
+		name: "rotations"
+	});
 	// Rotate in place
-	path.rotate(3*Math.PI/2);
-	// Continue upwards to .source
-	path.lineTo(2400, -700);
+	path.rotate(3*Math.PI/2, {
+		name: "rotations-rotated"
+	});
+	// Continue upwards to 'source'
+	path.lineTo(2400, -700, {
+		name: "source"
+	});
 	// Small arc downwards
 	path.arc(2250, -700, 150, 0, -Math.PI/2, true);
 
-	//Line to .follow
-	path.lineTo(1350, -850);
+	//Line to 'follow'
+	path.lineTo(1350, -850, {
+		name: "follow"
+	});
 	// Arc and rotate back to the beginning.
-	path.arc(1300, 50, 900, -Math.PI/2, -Math.PI, true, {rotate: Math.PI*2});
+	path.arc(1300, 50, 900, -Math.PI/2, -Math.PI, true, {rotate: Math.PI*2, name: "end"});
 
-	// We're done with the path, initate the plugin on our wrapper element
+	// We're done with the path, let's initate the plugin on our wrapper element
 	$(".wrapper").scrollPath({drawPath: true, wrapAround: true});
+
+	// Add scrollTo on click on the navigation anchors
+	$(".navigation").find("a").each(function() {
+		var target = this.getAttribute("href").replace("#", "");
+		$(this).click(function(e) {
+			e.preventDefault();
+			
+			// Include the jQuery easing plugin (http://gsgd.co.uk/sandbox/jquery/easing/)
+			// for extra easing functions like the one below
+			$.fn.scrollPath("scrollTo", target, 1000, "easeInOutSine");
+		});
+	});
 
 	/* ===================================================================== */
 

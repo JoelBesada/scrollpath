1d76e71ab687825bc45f3d052da66f0c5dfdad5b
Joel Besada <joel.besada@gmail.com>
2012-02-20 00:57:39 +0100

    Updated to v1.1.1

--- script/jquery.scrollpath.js
+++ script/jquery.scrollpath.js
@@ -1,7 +1,7 @@
 /*
                 =============================
                   jQuery Scroll Path Plugin
-                            v1.1
+                            v1.1.1
 
                    Demo and Documentation:
                   http://joelb.me/scrollpath
